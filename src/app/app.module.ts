8dba3c396b89e4a4588173dcee7b2e202d610e52
Devzamora99 <cristianHernandezZ1999@gmail.com>
2023-02-14 20:02:00 -0600

    Navbar-Se crearon los componentes iniciales y se le dio el estilo a la barra de navegacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './layouts/navigation/navigation.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
